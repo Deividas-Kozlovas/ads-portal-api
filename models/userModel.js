fb3cfd0ce7eb284fba3c73c8f555ae2846359bf2
Deividas-Kozlovas <kozlovas.deividas@gmail.com>
2025-02-25 18:28:56 +0200

    add: user model

---
+++ models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "User name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "User email is required"],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, "Invalid email format"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password fo not match",
+    },
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.method.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.method.changePasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changeTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000);
+    return JWTTimestamp < changeTimestamp;
+  }
+
+  return false;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
